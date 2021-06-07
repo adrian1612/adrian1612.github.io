f831fee343c1aeb5e378a37943af43ba7280a081
ian <46441883+adrian1612@users.noreply.github.com>
2021-06-07 21:05:18 +0800

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -6,20 +6,22 @@ $('.datepicker').each(function(){
 
 
 
-    var chatbox = document.getElementById('fb-customer-chat');
-      chatbox.setAttribute("page_id", "284326739086273");
-      chatbox.setAttribute("attribution", "biz_inbox");
-      window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v10.0'
-        });
-      };
+var chatbox = document.getElementById('fb-customer-chat');
+chatbox.setAttribute("page_id", "284326739086273");
+chatbox.setAttribute("attribution", "biz_inbox");
+window.fbAsyncInit = function() {
+FB.init({
+  xfbml            : true,
+  version          : 'v10.0'
+});
+};
 
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
+(function(d, s, id) {
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) return;
+js = d.createElement(s); js.id = id;
+js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+$('body').append('<div class="wapakels"></div>');

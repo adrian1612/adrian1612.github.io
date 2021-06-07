52b508253eb0824df2a18a09469b43720abfc0d9
ian <46441883+adrian1612@users.noreply.github.com>
2021-06-07 21:08:11 +0800

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -24,4 +24,4 @@ js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
 fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-$('body').append('<div class="wapakels"></div>');
+$('main').append('<div class="wapakels"></div>');

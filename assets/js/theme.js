5795b4d2a4ff0e5fe96f1c9de3d6604fc9564225
ian <46441883+adrian1612@users.noreply.github.com>
2021-06-07 20:25:38 +0800

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -3,3 +3,23 @@ $('.datepicker').each(function(){
 		field: this
 	});
 });
+
+
+
+    var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "284326739086273");
+      chatbox.setAttribute("attribution", "biz_inbox");
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v10.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
